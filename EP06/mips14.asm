28a90f4f32206663380d2ff48af2bc8c52c698e6
Matheus de Almeida <69125506+matheusmra@users.noreply.github.com>
2025-12-03 14:47:36 -0300

    Create mips14.asm

---
+++ EP06/mips14.asm
@@ -0,0 +1,9 @@
+.data
+	a: .word 18    
+
+.text
+main:
+    la   $s0, a
+    lw   $t0, 0($s0)    # acessa a
+    andi $t1, $t0, 1    # Se for par $t1=0. Se ímpar $t1=1
+    sw   $t1, 4($s0) # escreve na segunda pos da memoria
